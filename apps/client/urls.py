7a558bab62d4dd691bce3c17357c51c627f2e601
Victor Pereira <victtor.vpb@gmail.com>
2022-07-17 19:07:13 -0300

    change title

--- apps/client/urls.py
+++ apps/client/urls.py
@@ -4,12 +4,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from apps.client import views
 
 urlpatterns = [
-    path("scheduler", views.SchedulerView.as_view(), name="scheduler"),
-    path(
-        "scheduler_presbitery",
-        views.SchedulerPresbiteryView.as_view(),
-        name="scheduler_presbitery",
-    ),
+    # path("scheduler", views.SchedulerView.as_view(), name="scheduler"),
+    # path(
+    #     "scheduler_presbitery",
+    #     views.SchedulerPresbiteryView.as_view(),
+    #     name="scheduler_presbitery",
+    # ),
     path("", views.Main.as_view(), name="main"),
     path("pdfs_views", views.PDFsVews.as_view(), name="pdfs_views"),
 ]
